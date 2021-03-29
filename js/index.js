7fed8446f053adcd74064c0d563e0a37b976e8c0
DanielPas-coder <irondanpas@gmail.com>
2021-03-29 15:16:11 +0000

    Repairing

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Clock 
+setInterval(updateClock, 1000);
+
+function updateClock() {
+    const clockContainer = document.querySelector('.clock');
+    clockContainer.innerText = new Date().toLocaleTimeString();
+}
+
+new ProductList(new Cart());
