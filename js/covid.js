a0f6aae95b364f2ba78df1fee39352f80d8ccd04
DanielPas-coder <81445115+DanielPas-coder@users.noreply.github.com>
2021-03-29 18:46:22 +0200

    Update covid.js

--- js/covid.js
+++ js/covid.js
@@ -1,7 +1,7 @@
-alert("Hello!");
-alert("Check your covid results.");
-alert("If you have Covid-19, go back and come back when you were health.");
-alert("Oh, whatever.");
-alert("In internet it doesn't matter.");
-alert("Especially in wallpapers shop.");
-alert("Just buy something.");
+// alert("Hello!");
+// alert("Check your covid results.");
+// alert("If you have Covid-19, go back and come back when you were health.");
+// alert("Oh, whatever.");
+// alert("In internet it doesn't matter.");
+// alert("Especially in wallpapers shop.");
+// alert("Just buy something.");
